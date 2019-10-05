93b1c64548293e2c38ce31c7b77b2f4ccfb3cec5
Monica Scroggs <scrmr0210@gmail.com>
2019-10-04 21:36:44 -0400

    created html file, coped reset into reset.css, started first task in javascript for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//create random generator for numbers that the computer goes through: array?
+// computer chooses one of those alphabet variables
+// user can click any keyboard letter (guess), guess count documents the letter guessed 
+// if user guess=computer answer, then true
+// if true then wins score i++, AND start game over again
+/* if false then guesses left i--,
+    until guesses left=0, then losses=i++ and game restarts);*/
+
+let alphabet= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ]
+
