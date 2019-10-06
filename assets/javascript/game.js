5c429f9fbff2efc59bb9d013b89af8303ddec2a7
Monica Scroggs <scrmr0210@gmail.com>
2019-10-05 21:34:21 -0400

    still working on javascript, not showing up in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,64 @@
 // if true then wins score i++, AND start game over again
 /* if false then guesses left i--,
     until guesses left=0, then losses=i++ and game restarts);*/
-
+    
 let alphabet= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
 
-let computerguess = compRandletter(alphabet);
 
-function compRandletter(alphabet) {
-    console.log (alphabet[Math.floor(Math.random() * alphabet.length)]);
- };
- 
- compRandletter(alphabet); 
+let wins = document.getElementById('#wins')
+let winScore = 0;
 
+let losses = document.getElementById('#losses')
+let lossesScore = 0;
+
+let GuessesLeft = document.getElementById('#GuessesLeft')
+let GuessesLeftScore = 9;
+
+let GuessChoices = document.getElementById('#GuessChoices')
+let GuessChoicesScore = [];
+
+let scoreCounter = function () {
+    wins.innerHTML = "Wins: " + winScore;
+    losses.innerHTML = "Losses: " + lossesScore;
+    GuessesLeft.innerHTML = "Guesses Left: " + GuessesLeftScore;
+    GuessChoices.innerHTML = "Your Guesses So Far" + GuessChoicesScore
+}
+
+document.onkeyup = function(event) {
+
+    let userGuess = event.key;
+
+    let computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    let options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+
+   if (options.indexOf(userGuess) > -1) {
+
+       if (userGuess === computerGuess) {
+           wins++;
+           GuessesLeft = 9;
+           GuessChoices = [];
+           console.log(options.indexOf(userGuess))
+       }
+
+       if (userGuess != computerGuess) {
+           GuessesLeft --;
+           
+       }
+
+       if (GuessesLeft === 0) {
+
+       GuessesLeft = 9;
+       losses ++;
+       GuessChoices = [];
  
+   }
+
+   document.getElementById('')
+
+
+   }
+};
+
+//Need to either fix html link or .innerHTML to get my functions to show up 
